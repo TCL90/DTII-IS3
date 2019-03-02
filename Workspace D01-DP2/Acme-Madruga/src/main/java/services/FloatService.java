eb4d3471b9fb5b05d498ecf7bb3761076c990232
josmunpen <josemamup@gmail.com>
2019-03-02 14:07:20 +0100

    Problemas del último arreglados

--- Workspace D01-DP2/Acme-Madruga/src/main/java/services/FloatService.java
+++ Workspace D01-DP2/Acme-Madruga/src/main/java/services/FloatService.java
@@ -10,6 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 
 import repositories.FloatRepository;
+import domain.Brotherhood;
 import domain.Float;
 
 @Service
@@ -18,7 +19,10 @@ public class FloatService {
 
 	//Managed repository
 	@Autowired
-	private FloatRepository	floatRepository;
+	private FloatRepository		floatRepository;
+
+	@Autowired
+	private BrotherhoodService	brotherhoodService;
 
 
 	//Simple CRUD Methods
@@ -32,9 +36,14 @@ public class FloatService {
 
 	public Float save(final Float f) {
 		Assert.notNull(f);
+
+		if (f.getId() == 0) {
+			final Brotherhood logBro = this.brotherhoodService.findByPrincipal();
+			f.setBrotherhood(logBro);
+		}
+
 		return this.floatRepository.save(f);
 	}
-
 	public Float findOne(final Integer id) {
 		Assert.isTrue(id != 0);
 		return this.floatRepository.findOne(id);
