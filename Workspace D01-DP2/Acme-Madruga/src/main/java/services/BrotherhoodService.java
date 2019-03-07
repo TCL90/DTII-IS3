ee83441affaa0d33ee2893782e7c9ac73d10cf1d
Tomás Cabello López <tomascl1998@gmail.com>
2019-03-07 15:26:14 +0100

    Ultio update de enrolement

--- Workspace D01-DP2/Acme-Madruga/src/main/java/services/BrotherhoodService.java
+++ Workspace D01-DP2/Acme-Madruga/src/main/java/services/BrotherhoodService.java
@@ -186,7 +186,6 @@ public class BrotherhoodService {
 
 	public void checkBrotherhood(final Enrolement enrolement) {
 		final Brotherhood b = this.findOnePrincipal();
-		Assert.isTrue(enrolement.getStatus().contains("APPROVED"));
 		Assert.isTrue(enrolement.getBrotherhood().getId() == b.getId());
 
 	}
